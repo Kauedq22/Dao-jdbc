8d36f14105f76631b076b2a636923aa034fe90bc
Kauedq22 <kauedq12@gmail.com>
2023-10-24 18:36:20 -0300

    Classe Department

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,35 +1,12 @@
 package application;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-
-import db.DB;
-import db.DbIntegrityException;
+import model.entities.Department;
 
 public class Program {
 	public static void main(String[] args) {
-		Connection conn = null;
-		PreparedStatement st = null;
-		try {
-			conn = DB.getConnection();
-			
-			st= conn.prepareStatement(
-					"delete from department "
-					+ "where "
-					+ "Id = ?");
-			st.setInt(1, 5);
-			
-			int rowsAfecceted = st.executeUpdate();
-			
-			System.out.println("Done! rows affected: " + rowsAfecceted);
-			
-		}catch (SQLException e) {
-			throw new DbIntegrityException(e.getMessage());
-		}
-		finally {
-			DB.closeStatement(st);
-			DB.closeConnection();
-		}
+
+		Department obj = new Department(1, "books");
+		System.out.println(obj);
+
 	}
 }
